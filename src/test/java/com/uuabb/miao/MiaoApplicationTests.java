00f333660b8316145667c10b9757a7ac7358d6f9
yzbbanban <yzbbanban@live.com>
2018-03-22 21:20:02 +0800

    修改 test

--- src/test/java/com/uuabb/miao/MiaoApplicationTests.java
+++ src/test/java/com/uuabb/miao/MiaoApplicationTests.java
@@ -9,8 +9,37 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MiaoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        String s1 = "Programming";
+        String s2 = new String("Programming");
+        String s3 = "Program" + "ming";
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s1 == s2.intern());
+
+        try {
+            try {
+                throw new Sneeze();
+            }
+            catch ( Annoyance a ) {
+                System.out.println("Caught Annoyance");
+                throw a;
+            }
+        }
+        catch ( Sneeze s ) {
+            System.out.println("Caught Sneeze");
+            return ;
+        }
+        finally {
+            byte[] a=new byte[]{1,2,3,4,5};
+            System.out.println("a-- "+a);
+            System.out.println("Hello World!");
+        }
+
+    }
+
 
 }
+class Annoyance extends Exception {}
+class Sneeze extends Annoyance {}
