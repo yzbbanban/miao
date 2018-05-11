4cf5d72d4e05698f058fbb5ee080d5caf92237ce
yzbbanban <yzbbanban@live.com>
2018-05-12 00:22:35 +0800

    设置搜索功能

--- src/main/java/com/uuabb/miao/utils/RedisUtils.java
+++ src/main/java/com/uuabb/miao/utils/RedisUtils.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -19,6 +20,7 @@ public class RedisUtils {
 
     /**
      * 写入缓存
+     *
      * @param key
      * @param value
      * @return
@@ -34,13 +36,15 @@ public class RedisUtils {
         }
         return result;
     }
+
     /**
      * 写入缓存设置时效时间
+     *
      * @param key
      * @param value
      * @return
      */
-    public boolean set(final String key, Object value, Long expireTime ,TimeUnit timeUnit) {
+    public boolean set(final String key, Object value, Long expireTime, TimeUnit timeUnit) {
         boolean result = false;
         try {
             ValueOperations<Serializable, Object> operations = redisTemplate.opsForValue();
@@ -55,27 +59,30 @@ public class RedisUtils {
 
     /**
      * 增加
+     *
      * @param key
      * @param value
      * @return
      */
-    public Long incr(String key,long value) {
-        return redisTemplate.opsForValue().increment(key,value);
+    public Long incr(String key, long value) {
+        return redisTemplate.opsForValue().increment(key, value);
     }
 
 
     /**
      * 增加浮点数
+     *
      * @param key
      * @param value
      * @return
      */
-    public Double incr(String key,double value) {
-        return redisTemplate.opsForValue().increment(key,value);
+    public Double incr(String key, double value) {
+        return redisTemplate.opsForValue().increment(key, value);
     }
 
     /**
      * 批量删除对应的value
+     *
      * @param keys
      */
     public void remove(final String... keys) {
@@ -86,16 +93,19 @@ public class RedisUtils {
 
     /**
      * 批量删除key
+     *
      * @param pattern
      */
     public void removePattern(final String pattern) {
         Set<Serializable> keys = redisTemplate.keys(pattern);
-        if (keys.size() > 0){
+        if (keys.size() > 0) {
             redisTemplate.delete(keys);
         }
     }
+
     /**
      * 删除对应的value
+     *
      * @param key
      */
     public void remove(final String key) {
@@ -103,15 +113,20 @@ public class RedisUtils {
             redisTemplate.delete(key);
         }
     }
+
     /**
      * 判断缓存中是否有对应的value
+     *
      * @param key
      * @return
      */
-    public boolean exists(final String key) { return redisTemplate.hasKey(key);
+    public boolean exists(final String key) {
+        return redisTemplate.hasKey(key);
     }
+
     /**
      * 读取缓存
+     *
      * @param key
      * @return
      */
@@ -124,164 +139,201 @@ public class RedisUtils {
 
     /**
      * 哈希 添加
+     *
      * @param key
-     * @param hashKey
-     * @param value
+     * @param maps
      */
-    public void hmSet(final String key, Object hashKey, Object value){
+    public void hmSet(final String key, Map<String, Object> maps) {
         HashOperations<String, Object, Object> hash = redisTemplate.opsForHash();
-        hash.put(key,hashKey,value);
+//        hash.put(key, hashKey, value);
+        hash.putAll(key, maps);
     }
 
     /**
      * 哈希获取数据
+     *
      * @param key
      * @param hashKey
      * @return
      */
-    public Object hmGet(final String key, Object hashKey){
-        HashOperations<String, Object, Object>  hash = redisTemplate.opsForHash();
-        return hash.get(key,hashKey);
+    public Object hmGetValue(final String key, Object hashKey) {
+        HashOperations<String, Object, Object> hash = redisTemplate.opsForHash();
+        return hash.get(key, hashKey);
+    }
+
+    /**
+     * 哈希获取数据
+     *
+     * @param key
+     * @return
+     */
+    public Object hmGet(final String key) {
+        HashOperations<String, Object, Object> hash = redisTemplate.opsForHash();
+        return hash.entries(key);
     }
 
     /**
      * 列表添加
+     *
      * @param k
      * @param v
      */
-    public void lPush(final String k,Object v){
+    public void lPush(final String k, Object v) {
         ListOperations<String, Object> list = redisTemplate.opsForList();
-        list.rightPush(k,v);
+        list.rightPush(k, v);
     }
 
     /**
      * 移除
+     *
      * @param key
      * @param item
      * @return
      */
-    public Long hmRemove(final String key, Object... item){
-        return redisTemplate.opsForHash().delete(key,item);
+    public Long hmRemove(final String key, Object... item) {
+        return redisTemplate.opsForHash().delete(key, item);
     }
 
     /**
      * 列表获取
+     *
      * @param k
      * @param l
      * @param l1
      * @return
      */
-    public List<Object> lRange(final String k, long l, long l1){
+    public List<Object> lRange(final String k, long l, long l1) {
         ListOperations<String, Object> list = redisTemplate.opsForList();
-        return list.range(k,l,l1);
+        return list.range(k, l, l1);
     }
 
     /**
      * 集合添加
+     *
      * @param key
      * @param value
      */
-    public void add(final String key,Object value){
+    public void add(final String key, Object value) {
         SetOperations<String, Object> set = redisTemplate.opsForSet();
-        set.add(key,value);
+        set.add(key, value);
     }
 
     /**
      * 集合获取
+     *
      * @param key
      * @return
      */
-    public Set<Object> setMembers(final String key){
+    public Set<Object> getMembers(final String key) {
         SetOperations<String, Object> set = redisTemplate.opsForSet();
         return set.members(key);
     }
 
     /**
      * 有序集合添加
+     *
      * @param key
      * @param value
      * @param scoure
      */
-    public void zAdd(final String key,Object value,double scoure){
+    public void zAdd(final String key, Object value, double scoure) {
         ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
-        zset.add(key,value,scoure);
+        zset.add(key, value, scoure);
     }
 
     /**
      * 获取下标集合（正序）
+     *
      * @param key
      * @param from
      * @param to
      * @return
      */
-    public Set<Object> zRange(final String key,long from ,long to){
-        return  redisTemplate.opsForZSet().range(key,from,to);
+    public Set<Object> zRange(final String key, long from, long to) {
+        return redisTemplate.opsForZSet().range(key, from, to);
     }
 
     /**
      * 有序集合获取,根据 score 区间获取
+     *
      * @param key
      * @param score
      * @param score1
      * @return
      */
-    public Set<Object> rangeByScore(final String key, double score, double score1){
+    public Set<Object> rangeByScore(final String key, double score, double score1) {
         ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
         return zset.rangeByScore(key, score, score1);
     }
 
     /**
      * 计算有序集合的数量
+     *
      * @param key
      * @return
      */
-    public Long zCard(final String key){
+    public Long zCard(final String key) {
         return redisTemplate.opsForZSet().zCard(key);
     }
 
     /**
      * 删除有序集合区间数据
+     *
      * @param key
      * @param from
      * @param to
      */
-    public Long zRemRangeByRank(final String key, long from, long to){
-        return redisTemplate.opsForZSet().removeRange(key,from,to);
+    public Long zRemRangeByRank(final String key, long from, long to) {
+        return redisTemplate.opsForZSet().removeRange(key, from, to);
     }
 
-    public Double zScore(final String key,Object obj){
-        return  redisTemplate.opsForZSet().score(key,obj);
+    public Double zScore(final String key, Object obj) {
+        return redisTemplate.opsForZSet().score(key, obj);
     }
 
     /**
      * 删除多个有序集合
+     *
      * @param key
      * @param var2
      * @return
      */
-    public Long zRemove(final String key,Object... var2){
-        return redisTemplate.opsForZSet().remove(key,var2);
+    public Long zRemove(final String key, Object... var2) {
+        return redisTemplate.opsForZSet().remove(key, var2);
     }
 
     /**
      * 根据下标获取所有有序集合（倒序大到小）
+     *
      * @param key
      * @param start
      * @param end
      * @return
      */
-    public Set<Object> zRevRange(final String key,long start,long end){
-        return redisTemplate.opsForZSet().reverseRange(key,start,end);
+    public Set<Object> zRevRange(final String key, long start, long end) {
+        return redisTemplate.opsForZSet().reverseRange(key, start, end);
     }
 
     /**
      * 分值自增
+     *
      * @param key
-     * @param obj 自增列
+     * @param obj   自增列
      * @param index 增加数
      */
-    public void zIncrBy(final String key, Object obj,double index){
-        redisTemplate.opsForZSet().incrementScore(key,obj,index);
+    public void zIncrBy(final String key, Object obj, double index) {
+        redisTemplate.opsForZSet().incrementScore(key, obj, index);
+    }
+
+    /**
+     * 事务
+     *
+     * @param key
+     * @param obj   自增列
+     * @param index 增加数
+     */
+    public void pipeline(final String key, Object obj, double index) {
+//        redisTemplate.executePipelined();
     }
 
 
--- src/test/java/com/uuabb/miao/dao/RedisTest.java
+++ src/test/java/com/uuabb/miao/dao/RedisTest.java
@@ -4,6 +4,7 @@ import com.uuabb.miao.service.IAreaService;
 import com.uuabb.miao.service.impl.AreaServiceImpl;
 import com.uuabb.miao.entity.Area;
 import com.uuabb.miao.utils.RedisUtils;
+import org.assertj.core.util.Maps;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -13,9 +14,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by brander on 2018/3/27
@@ -38,55 +40,55 @@ public class RedisTest {
 
     /**
      * @Date:10:11 2017/12/20
-     *查询数据
-     *并将数据数据存在redis里
+     * 查询数据
+     * 并将数据数据存在redis里
      */
     @Test
     public void testfind() {
         //如果缓存存在
         boolean hasKey = redisUtils.exists(key.concat("1011"));
-        if(hasKey){
+        if (hasKey) {
             //获取缓存
-            Object object =  redisUtils.get(key.concat("1011"));
-            logger.info("从缓存获取的数据"+ object);
-        }else{
+            Object object = redisUtils.get(key.concat("1011"));
+            logger.info("从缓存获取的数据" + object);
+        } else {
             //从DB中获取信息
             logger.info("从数据库中获取数据");
             Area area = areaService.getAreaById(2);//根据ID查询，因为是测试，就直接写了id为1011
             System.out.println(area);
             //数据插入缓存（set中的参数含义：key值，user对象，缓存存在时间10（long类型），时间单位）
-            redisUtils.set(key.concat(area.getAreaId().toString()),area,10L, TimeUnit.MINUTES);
-            logger.info("数据插入缓存"+area.toString());
+            redisUtils.set(key.concat(area.getAreaId().toString()), area, 10L, TimeUnit.MINUTES);
+            logger.info("数据插入缓存" + area.toString());
         }
 
     }
 
     @Test
-    public void testCache(){
-        String s= (String) redisUtils.get("getAreaList()");
+    public void testCache() {
+        String s = (String) redisUtils.get("getAreaList()");
         System.out.println(s);
     }
+
     @Test
-    public void testPattern(){
-        stringRedisTemplate.keys("*"+"ss"+"*");
-        String s= (String) redisUtils.get("getAreaList()");
+    public void testPattern() {
+        stringRedisTemplate.keys("*" + "ss" + "*");
+        String s = (String) redisUtils.get("getAreaList()");
         System.out.println(s);
     }
 
     /**
      * @Date:10:11 2017/12/20
-     *删除数据
-     *
+     * 删除数据
      */
     @Test
-    public void testdel(){
+    public void testdel() {
         //缓存存在
         boolean hasKey = redisUtils.exists(key.concat("1011"));
-        if(hasKey){
-            Object object =  redisUtils.get(key.concat("1011"));
+        if (hasKey) {
+            Object object = redisUtils.get(key.concat("1011"));
             redisUtils.remove(key.concat("1011"));
             logger.info("从缓存中删除数据");
-        }else {
+        } else {
             logger.info("缓存中没有数据！");
         }
     }
@@ -95,37 +97,116 @@ public class RedisTest {
     private static final String POSTS_ID = "posts:id";
 
     @Test
-    public void testHash(){
-        Long id=0L;
-        String.format(POSTS_DATA,"");
-        String token= UUID.randomUUID().toString();
-        long timestamp=System.currentTimeMillis()/1000;
-        String key="user:";
+    public void testHash() {
+        Long id = 0L;
+        String.format(POSTS_DATA, "");
+        String token = UUID.randomUUID().toString();
+        long timestamp = System.currentTimeMillis() / 1000;
+        String key = "user:";
 //        addHash("user:",token,timestamp);
 //        removeLimit(key);
-        Set<Object> set=redisUtils. zRevRange(key,1,-1);
-        System.out.println("===set===> "+set);
+        Set<Object> set = redisUtils.zRevRange(key, 1, -1);
+        System.out.println("===set===> " + set);
     }
 
-    private void addHash(String key,String token,long timestamp){
+    private void addHash(String key, String token, long timestamp) {
 
-        redisUtils.zAdd(key,token,timestamp);
+        redisUtils.zAdd(key, token, timestamp);
     }
 
-    private void removeLimit(String key){
-        long limit=redisUtils.zRemRangeByRank(key,0,1);
-        System.out.println("=======> "+limit);
+    private void removeLimit(String key) {
+        long limit = redisUtils.zRemRangeByRank(key, 0, 1);
+        System.out.println("=======> " + limit);
     }
 
 
     @Test
-    public void testString(){
-        String key="ban";
-        redisUtils.set(key,2);
+    public void testString() {
+        String key = "ban";
+        redisUtils.set(key, 2);
 
         Object s = redisUtils.get(key);
-        System.out.println("-----key----->"+s);
-        redisUtils.incr(key,3L);
+        System.out.println("-----key----->" + s);
+        redisUtils.incr(key, 3L);
+
+
     }
+
+    @Test
+    public void pattern() {
+//        redisUtils.sadd("banban", "ban_1");
+//        redisUtils.sadd("banban", "yang_2");
+//        redisUtils.sadd("banban", "keke_3");
+//        redisUtils.sadd("banban", "cici_4");
+//        redisUtils.sadd("banban", "vovo_5");
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("id", "5");
+//        map.put("name", "vovo");
+//        map.put("title", "vovoO(∩_∩)O~~");
+//        redisUtils.hmSet("map5", map);
+        Set<Object> names = redisUtils.getMembers("banban");
+        System.out.println(names);
+        Iterator<Object> it = names.iterator();
+        String name = "c";
+        Set<Object> result = new HashSet<>();
+        while (it.hasNext()) {
+            String str = String.valueOf(it.next());
+            if (str.contains(name)) {
+                str = str.split("_")[1];
+                result.add(str);
+            }
+        }
+
+        System.out.println(result);
+        Iterator<Object> reit = result.iterator();
+        while (reit.hasNext()){
+            System.out.println(redisUtils.hmGet("map"+String.valueOf(reit.next())));
+        }
+
+
+    }
+
+
+    private static final Pattern QUERY_RE = Pattern.compile("[+-]?[a-z']{2,}");
+    private static final Set<String> STOP_WORDS = new HashSet<String>();
+
+    public class Query {
+        public final List<List<String>> all = new ArrayList<List<String>>();
+        public final Set<String> unwanted = new HashSet<String>();
+    }
+
+    public Query parse(String queryString) {
+        Query query = new Query();
+        Set<String> current = new HashSet<String>();
+        Matcher matcher = QUERY_RE.matcher(queryString.toLowerCase());
+        while (matcher.find()) {
+            String word = matcher.group().trim();
+            char prefix = word.charAt(0);
+            if (prefix == '+' || prefix == '-') {
+                word = word.substring(1);
+            }
+
+            if (word.length() < 2 || STOP_WORDS.contains(word)) {
+                continue;
+            }
+
+            if (prefix == '-') {
+                query.unwanted.add(word);
+                continue;
+            }
+
+            if (!current.isEmpty() && prefix != '+') {
+                query.all.add(new ArrayList<String>(current));
+                current.clear();
+            }
+            current.add(word);
+        }
+
+        if (!current.isEmpty()) {
+            query.all.add(new ArrayList<String>(current));
+        }
+        return query;
+    }
+
 
 }
