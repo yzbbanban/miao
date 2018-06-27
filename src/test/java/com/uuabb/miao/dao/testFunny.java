0b3a8b471a503a159efa11302cac08a5cb12f69e
wangban <yzbbanban@live.com>
2018-06-27 16:59:40 +0800

    图片图片超好玩

---
+++ src/test/java/com/uuabb/miao/dao/testFunny.java
@@ -0,0 +1,117 @@
+package com.uuabb.miao.dao;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by ban on 2018/6/27.
+ */
+public class testFunny {
+    /**
+     * @param path
+     *            图片路径
+     */
+    public static void createAsciiPic(final String path) throws Exception {
+        final String base = "@#&$%*o!;.";// 字符串由复杂到简单
+        try {
+            BufferedImage image = ImageIO.read(new File(path));
+            image = zoomImage(image, image.getWidth() / 10, image.getHeight() / 10);
+
+            String str = "";
+            for (int y = 0; y < image.getHeight(); y+=2) {
+                if (y == 0) {
+                    System.out.println("/**");
+                } else if (!"".equals(str.trim())) {
+                    System.out.print(" *" + str);
+                    System.out.println();
+                }
+                str = "";
+                for (int x = 0; x < image.getWidth(); x++) {
+                    final int pixel = image.getRGB(x, y);
+                    final int r = (pixel & 0xff0000) >> 16, g = (pixel & 0xff00) >> 8, b = pixel & 0xff;
+                    final float gray = 0.299f * r + 0.578f * g + 0.114f * b;
+                    final int index = Math.round(gray * (base.length() + 1) / 255);
+                    str += index >= base.length() ? " " : String.valueOf(base.charAt(index));
+//                    System.out.print(str);
+                }
+            }
+            System.out.println(" */");
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * 图片缩放,w，h为缩放的目标宽度和高度
+     * src为源文件目录，dest为缩放后保存目录
+     */
+    public static BufferedImage zoomImage(BufferedImage image,int w,int h) throws Exception {
+
+        double wr=0,hr=0;
+
+        BufferedImage bufImg = image; //读取图片
+        Image item = bufImg.getScaledInstance(w, h, bufImg.SCALE_SMOOTH);//设置缩放目标图片模板
+
+        wr = w * 4.0 / bufImg.getWidth();     //获取缩放比例
+        hr= h * 3.0 / bufImg.getHeight();
+
+        AffineTransformOp ato = new AffineTransformOp(AffineTransform.getScaleInstance(wr, hr), null);
+        item = ato.filter(bufImg, null);
+
+        return toBufferedImage(item);
+    }
+
+    /**
+     * 图片转换
+     * @param image
+     * @return
+     */
+    public static BufferedImage toBufferedImage(Image image) {
+        if (image instanceof BufferedImage) {
+            return (BufferedImage)image;
+        }
+        // 加载所有像素
+        image = new ImageIcon(image).getImage();
+        BufferedImage bimage = null;
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        try {
+            int transparency = Transparency.OPAQUE;
+            // 创建buffer图像
+            GraphicsDevice gs = ge.getDefaultScreenDevice();
+            GraphicsConfiguration gc = gs.getDefaultConfiguration();
+            bimage = gc.createCompatibleImage(
+                    image.getWidth(null), image.getHeight(null), transparency);
+        } catch (HeadlessException e) {
+            e.printStackTrace();
+        }
+        if (bimage == null) {
+            int type = BufferedImage.TYPE_INT_RGB;
+            bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), type);
+        }
+        // 复制
+        Graphics g = bimage.createGraphics();
+        // 赋值
+        g.drawImage(image, 0, 0, null);
+        g.dispose();
+        return bimage;
+    }
+    /**
+     * test
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+        try {
+            testFunny.createAsciiPic("F:\\其他人员\\圆圈\\TIM图片20180627141432.jpg");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
